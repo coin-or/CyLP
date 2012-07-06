Performance: symmetrisizing the Hessian, Bug fixes

py/QP/QP.py
@@ -328,8 +328,15 @@ class QP:
         start = clock()
         A = self.A
         b = CyLPArray(self.b)
-        c = CyLPArray(self.c)
+        c = CyLPArray(self.c) 
         G = self.G
+        
+#        print 'A\n', A.todense()
+#        print 'b\n', b
+#        print 'c\n', c
+#        print 'G\n', G.todense()
+#
+
         
         nVar = self.n
         nx = self.nOriginalVar
@@ -359,16 +366,33 @@ class QP:
 
         #print G.todense()
         minDiag = min(G[i, i] for i in xrange(nx))    
-        delta =  max(10**-8, 0.1 * max(1.0**-4, minDiag))
+        delta =  max(10**-8, 0.01 * max(1.0**-4, minDiag))
         G = G + delta * I(nVar)
         #print G.todense()
-         
+        
+#        print 'G'
+#        print G.todense()
+#
+#        print 'A'
+#        print A.todense()
+#
+#        print 'A.T'
+#        print A.T.todense()
+#
+#        print 'A.T[:nx, :]'
+#        print A.T[:nx, :].todense()
+#
+#        print 'A.T[nx:, :]'
+#        print A.T[nx:, :].todense()
+#   
+#        print nx
+
         s += G[:nx, :] * x - A.T[:nx, :] * y + sp[:nx] - sm[:nx] == -c[:nx]
         if nSlacks:
             s += -A.T[nx:, :] * y - z + sp[nx:] - sm[nx:] == 0
             s += z >= 0
         
-        s += A * x == b
+        #s += A * x == b
 
         s += x[nx:] >= 0
         s += sp >= 0
@@ -396,7 +420,36 @@ class QP:
         timeToSolve = clock() - start
 
         self.writeReport('qpout', s, timeToMake, timeToSolve, method, p)
-
+        
+#        print 'x'
+#        print s.primalVariableSolution['x']
+#        print 'z'
+#        print s.primalVariableSolution['z']
+#        print 'y'
+#        print s.primalVariableSolution['y']
+#        
+#        nx = self.nOriginalVar
+#        x = np.matrix(s.primalVariableSolution['x']).T
+#        y = np.matrix(s.primalVariableSolution['y']).T
+#        z = np.matrix(s.primalVariableSolution['z']).T
+#        #x = x[:nx]
+#        #G = self.G[:nx, :nx]
+#        c = np.matrix(self.c).T
+#        print c
+#        print np.zeros((3, 1))
+#        c = np.concatenate((c, np.zeros((5, 1))), axis=0)
+#        print c
+#
+#        print 'A*x = '
+#        print A * x
+#        print 'G * x + c'
+#        print G * x + c
+#        print 'A.T * y'
+#        print A.T * y
+#
+#        print G[:nx, :] * x - A.T[:nx, :] * y
+#        print -A.T[nx:, :] * y - z
+#        #print G * x + c - A.T * y - z  
         return
 
 #        m = CyLPModel()

py/modeling/CyLPModel.py
@@ -682,7 +682,18 @@ class IndexFactory:
         for cname, rg in cind.items():
             s += '%s : %s\n' % (cname.rjust(15), str(rg))
         return s
-
+    
+    def reverseVarSearch(self, ind):
+        '''
+        Take an index and return the corresponding variable name. 
+        '''
+        inds = self.varIndex
+        for key in inds.keys():
+            if ind in inds[key]:
+                i = np.where(inds[key]==ind)[0][0]
+                return key, i
+        return -1, -1
+            
 
 from CyLP.py.utils.sparseUtil import sparseConcat, csr_matrixPlus, csc_matrixPlus
 

py/pivots/WolfePivotPE.py
@@ -36,7 +36,7 @@ class WolfePivotPE(PivotPythonBase):
         self.compCount = 0
         self.nonCompCount = 0
         self.compRej = 0
-
+        self.numberOfIncompSinceLastUpdate = 0
     # Begining of Positive-Edge-related attributes
     
     def updateP(self):
@@ -117,20 +117,53 @@ class WolfePivotPE(PivotPythonBase):
 
             comp_rc = abs(rc[comp_varInds])
             if len(comp_rc) > 0:
-                maxCompInd = comp_varInds[np.argmax(comp_rc)]
-                maxCompRc = rc[maxCompInd]
+                if False:
+                    s = self.clpModel
+                    cl = self.complementarityList
+                    maxCompRc = 0
+                    count = 0
+                    #randinds = np.random.randint(len(comp_varInds), size=10)
+                    nn = 5
+#                    for i in xrange(random.randint(0, 
+#                                    len(comp_varInds)), 
+#                                    min(nn, len(comp_varInds))):
+                    for i in xrange(min(nn, len(comp_varInds))):
+                    #for i in randinds:
+                        ind = comp_varInds[i]
+                        if (s.getVarStatus(cl[ind]) != 1 and 
+                                   comp_rc[i] > maxCompRc):
+                            maxCompInd = ind
+                            maxCompRc = rc[maxCompInd]
+                else:
+                    maxCompInd = comp_varInds[np.argmax(comp_rc)]
+                    maxCompRc = rc[maxCompInd]
+            del compatibility
+            del comp_rc
+            del comp_varInds
 
         if len(rc2) > 0:
             maxInd = indicesToConsider[np.argmax(rc2)]
             maxRc = rc[maxInd]
-
+        
         del rc2
         if maxCompInd != -1 and abs(maxCompRc) > 0.1 * abs(maxRc):
             self.compCount += 1
+            #print s.getVarNameByIndex(maxCompInd)
             return maxCompInd
+        
         self.nonCompCount += 1
         self.CompIter = False
-        self.updateW()
+        if self.numberOfIncompSinceLastUpdate > 5:
+            self.updateW()
+            self.numberOfIncompSinceLastUpdate = 0
+        else:
+            self.numberOfIncompSinceLastUpdate += 1
+
+#        
+#        for i in xrange(s.nConstraints):
+#            print s.getVarNameByIndex(s.getPivotVariable()[i]),
+#        print
+        
         return maxInd
         
 #        