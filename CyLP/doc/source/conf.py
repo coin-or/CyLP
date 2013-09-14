976d3e25ec6bb252fc1b02ce0795d165bc2a6fc4
Mehdi Towhidi <mehdi.towhidi@gerad.ca>
2013-09-14 12:31:38 -0400

    conf.py path fix

--- CyLP/doc/source/conf.py
+++ CyLP/doc/source/conf.py
@@ -18,7 +18,7 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) +
-                                   '../../../')
+                                   '../../../'))
 #sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) +
 #                                    '../../../python/utils'))
 #sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + '../../../build/temp.macosx-10.6-universal-2.6'))
