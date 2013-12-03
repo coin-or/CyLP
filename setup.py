affc7ea44df947d31f388ae837f62bd5006b1854
Mehdi Towhidi <mehdi.towhidi@gerad.ca>
2013-12-03 00:53:42 -0500

    Bug fix: CbcSolver in ubuntu

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import Extension
 
 
 PROJECT = 'cylp'
-VERSION = '0.2.3.2'
+VERSION = '0.2.3.3'
 URL = 'https://github.com/mpy/cylp'
 AUTHOR_EMAIL = u'mehdi.towhidi@gerad.ca'
 DESC = 'A Python interface for CLP, CBC, and CGL'
@@ -62,6 +62,10 @@ elif 'win' in operatingSystem:
     operatingSystem = 'windows'
 
 libs = get_libs()
+#Take care of Ubuntu case
+if 'CbcSolver' not in libs:
+    libs.append('CbcSolver')
+
 libDirs = ['.', join('.', cythonFilesDir), join(CoinDir, 'lib'),
            join('.', cythonFilesDir), join(CoinDir, 'lib', 'intel')]
 includeDirs = [join('.', cppFilesDir), join('.', cythonFilesDir),
