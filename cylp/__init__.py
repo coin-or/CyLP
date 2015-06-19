4888250db467222e713bed5dffda5baaebcbc929
Ali Ebrahim <ali.ebrahim314@gmail.com>
2015-06-19 15:44:14 -0700

    Remove linebreak in __version__

--- cylp/__init__.py
+++ cylp/__init__.py
@@ -1,4 +1,4 @@
 import os
 from os.path import realpath, join
 currentDir = os.path.dirname(realpath(__file__))
-__version__ = open(join(currentDir, 'VERSION')).read()
+__version__ = open(join(currentDir, 'VERSION')).read().strip()
