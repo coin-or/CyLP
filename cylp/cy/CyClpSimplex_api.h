fafed02320d5c380b1f93eedd2203e4ed8a41a16
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-10-20 15:05:22 -0700

    cylp/*/CyClpSimplex_api.h: Remove Python 2 support

--- cylp/cy/CyClpSimplex_api.h
+++ cylp/cy/CyClpSimplex_api.h
@@ -13,11 +13,7 @@ static PyObject *__Pyx_ImportModule(const char *name) {
     PyObject *py_name = 0;
     PyObject *py_module = 0;
 
-    #if PY_MAJOR_VERSION < 3
-    py_name = PyString_FromString(name);
-    #else
     py_name = PyUnicode_FromString(name);
-    #endif
     if (!py_name)
         goto bad;
     py_module = PyImport_Import(py_name);
@@ -49,7 +45,6 @@ static int __Pyx_ImportFunction(PyObject *module, const char *funcname, void (**
                 PyModule_GetName(module), funcname);
         goto bad;
     }
-#if PY_VERSION_HEX >= 0x02070000 && !(PY_MAJOR_VERSION==3&&PY_MINOR_VERSION==0)
     if (!PyCapsule_IsValid(cobj, sig)) {
         PyErr_Format(PyExc_TypeError,
             "C function %s.%s has wrong signature (expected %s, got %s)",
@@ -57,21 +52,6 @@ static int __Pyx_ImportFunction(PyObject *module, const char *funcname, void (**
         goto bad;
     }
     tmp.p = PyCapsule_GetPointer(cobj, sig);
-#else
-    {const char *desc, *s1, *s2;
-    desc = (const char *)PyCObject_GetDesc(cobj);
-    if (!desc)
-        goto bad;
-    s1 = desc; s2 = sig;
-    while (*s1 != '\0' && *s1 == *s2) { s1++; s2++; }
-    if (*s1 != *s2) {
-        PyErr_Format(PyExc_TypeError,
-            "C function %s.%s has wrong signature (expected %s, got %s)",
-             PyModule_GetName(module), funcname, sig, desc);
-        goto bad;
-    }
-    tmp.p = PyCObject_AsVoidPtr(cobj);}
-#endif
     *f = tmp.fp;
     if (!(*f))
         goto bad;
